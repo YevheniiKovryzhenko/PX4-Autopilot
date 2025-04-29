3f5d39599646404f585fbf0a8e6be4cafa9d095a
Jack Kovryzhenko <yzk0058@auburn.edu>
2025-04-28 20:23:13 -0500

    ported custom uorb messages

--- src/modules/logger/logged_topics.cpp
+++ src/modules/logger/logged_topics.cpp
@@ -390,6 +390,182 @@ void LoggedTopics::add_mavlink_tunnel()
 	add_topic("mavlink_tunnel");
 }
 
+void LoggedTopics::add_sim_topics()
+{
+	add_topic("action_request");
+	add_topic("actuator_armed");
+	add_optional_topic("actuator_controls_status_0", 300);
+	add_topic("airspeed", 1000);
+	add_optional_topic("airspeed_validated", 200);
+	// add_optional_topic("autotune_attitude_control_status", 100);
+	// add_optional_topic("camera_capture");
+	// add_optional_topic("camera_trigger");
+	// add_topic("cellular_status", 200);
+	// add_topic("commander_state");
+	add_topic("cpuload");
+	add_optional_topic("external_ins_attitude");
+	add_optional_topic("external_ins_global_position");
+	add_optional_topic("external_ins_local_position");
+	add_optional_topic("esc_status", 250);
+	add_topic("failure_detector_status", 100);
+	add_topic("failsafe_flags");
+	// add_optional_topic("follow_target", 500);
+	// add_optional_topic("follow_target_estimator", 200);
+	// add_optional_topic("follow_target_status", 400);
+	// add_optional_topic("flaps_setpoint", 1000);
+	// add_topic("gimbal_manager_set_attitude", 500);
+	// add_optional_topic("generator_status");
+	add_optional_topic("gps_dump");
+	// add_optional_topic("gimbal_controls", 200);
+	// add_optional_topic("gripper");
+	// add_optional_topic("heater_status");
+	// add_topic("home_position");
+	add_topic("hover_thrust_estimate", 100);
+	add_topic("input_rc", 500);
+	// add_optional_topic("internal_combustion_engine_status", 10);
+	// add_optional_topic("iridiumsbd_status", 1000);
+	// add_optional_topic("irlock_report", 1000);
+	// add_optional_topic("landing_gear", 200);
+	// add_optional_topic("landing_gear_wheel", 100);
+	// add_optional_topic("landing_target_pose", 1000);
+	add_optional_topic("launch_detection_status", 200);
+	add_optional_topic("magnetometer_bias_estimate", 200);
+	add_topic("manual_control_setpoint", 200);
+	add_topic("manual_control_switches");
+	// add_topic("mission_result");
+	// add_topic("navigator_mission_item");
+	// add_topic("npfg_status", 100);
+	// add_topic("offboard_control_mode", 100);
+	add_topic("onboard_computer_status", 10);
+	add_topic("parameter_update");
+	// add_topic("position_controller_status", 500);
+	// add_topic("position_controller_landing_status", 100);
+	// add_topic("position_setpoint_triplet", 200);
+	// add_optional_topic("px4io_status");
+	add_topic("radio_status");
+	add_topic("rtl_time_estimate", 1000);
+	add_topic("sensor_combined");
+	add_optional_topic("sensor_correction");
+	add_optional_topic("sensor_gyro_fft", 50);
+	add_topic("sensor_selection");
+	add_topic("sensors_status_imu", 200);
+	// add_optional_topic("spoilers_setpoint", 1000);
+	add_topic("system_power", 500);
+	add_optional_topic("takeoff_status", 1000);
+	// add_optional_topic("tecs_status", 200);
+	// add_optional_topic("tiltrotor_extra_controls", 100);
+	// add_topic("trajectory_setpoint", 200);
+	add_topic("transponder_report");
+	add_topic("vehicle_acceleration", 50);
+	add_topic("vehicle_air_data", 200);
+	add_topic("vehicle_angular_velocity", 20);
+	add_topic("vehicle_attitude", 50);
+	// add_topic("vehicle_attitude_setpoint", 50);
+	add_topic("vehicle_command");
+	add_topic("vehicle_command_ack");
+	// add_topic("vehicle_constraints", 1000);
+	add_topic("vehicle_control_mode");
+	add_topic("vehicle_global_position", 200);
+	add_topic("vehicle_gps_position", 500);
+	add_topic("vehicle_land_detected");
+	add_topic("vehicle_local_position", 100);
+	// add_topic("vehicle_local_position_setpoint", 100);
+	add_topic("vehicle_magnetometer", 200);
+	// add_topic("vehicle_rates_setpoint", 20);
+	// add_topic("vehicle_roi", 1000);
+	add_topic("vehicle_status");
+	add_optional_topic("vtol_vehicle_status", 200);
+	add_topic("wind", 1000);
+
+	// multi topics
+	add_optional_topic_multi("actuator_outputs", 100, 3);
+	add_optional_topic_multi("airspeed_wind", 1000, 4);
+	add_optional_topic_multi("control_allocator_status", 200, 2);
+	// add_optional_topic_multi("rate_ctrl_status", 200, 2);
+	// add_optional_topic_multi("sensor_hygrometer", 500, 4);
+	add_optional_topic_multi("rpm", 200);
+	add_optional_topic_multi("telemetry_status", 1000, 4);
+
+	// EKF multi topics (currently max 9 estimators)
+// #if CONSTRAINED_MEMORY
+// 	static constexpr uint8_t MAX_ESTIMATOR_INSTANCES = 1;
+// #else
+// 	static constexpr uint8_t MAX_ESTIMATOR_INSTANCES = 6; // artificially limited until PlotJuggler fixed
+// 	add_optional_topic("estimator_selector_status");
+// 	add_optional_topic_multi("estimator_attitude", 500, MAX_ESTIMATOR_INSTANCES);
+// 	add_optional_topic_multi("estimator_global_position", 1000, MAX_ESTIMATOR_INSTANCES);
+// 	add_optional_topic_multi("estimator_local_position", 500, MAX_ESTIMATOR_INSTANCES);
+// 	add_optional_topic_multi("estimator_wind", 1000, MAX_ESTIMATOR_INSTANCES);
+// #endif
+
+	// // always add the first instance
+	// add_topic("estimator_baro_bias", 500);
+	// add_topic("estimator_gnss_hgt_bias", 500);
+	// add_topic("estimator_rng_hgt_bias", 500);
+	// add_topic("estimator_ev_pos_bias", 500);
+	// add_topic("estimator_event_flags", 0);
+	// add_topic("estimator_gps_status", 1000);
+	// add_topic("estimator_innovation_test_ratios", 500);
+	// add_topic("estimator_innovation_variances", 500);
+	// add_topic("estimator_innovations", 500);
+	// add_topic("estimator_optical_flow_vel", 200);
+	// add_topic("estimator_sensor_bias", 0);
+	// add_topic("estimator_states", 1000);
+	// add_topic("estimator_status", 200);
+	// add_topic("estimator_status_flags", 0);
+	// add_topic("yaw_estimator_status", 1000);
+
+	// // log all raw sensors at minimal rate (at least 1 Hz)
+	// add_topic_multi("battery_status", 200, 2);
+	// add_topic_multi("differential_pressure", 1000, 2);
+	// add_topic_multi("distance_sensor", 1000, 2);
+	// add_optional_topic_multi("sensor_accel", 1000, 4);
+	// add_optional_topic_multi("sensor_baro", 1000, 4);
+	// add_topic_multi("sensor_gps", 1000, 2);
+	// add_topic_multi("sensor_gnss_relative", 1000, 1);
+	// add_optional_topic_multi("sensor_gyro", 1000, 4);
+	// add_topic_multi("sensor_mag", 1000, 4);
+	// add_topic_multi("sensor_optical_flow", 1000, 2);
+
+	// add_topic_multi("vehicle_imu", 500, 4);
+	// add_topic_multi("vehicle_imu_status", 1000, 4);
+	// add_optional_topic_multi("vehicle_magnetometer", 500, 4);
+	// add_topic("vehicle_optical_flow", 500);
+	// //add_optional_topic("vehicle_optical_flow_vel", 100);
+	// add_optional_topic("pps_capture");
+
+	// // additional control allocation logging
+	// add_topic("actuator_motors", 100);
+	// add_topic("actuator_servos", 100);
+	// add_topic_multi("vehicle_thrust_setpoint", 20, 2);
+	// add_topic_multi("vehicle_torque_setpoint", 20, 2);
+
+	//get all the debug instances used for simulink:
+	int32_t sm_log_delay_ms = 0;
+	param_get(param_find("SM_LOG_DELAY"),&sm_log_delay_ms);
+	add_topic("simulink_inbound", sm_log_delay_ms);
+	add_topic("simulink_outbound", sm_log_delay_ms);
+	add_topic("simulink_outbound_1", sm_log_delay_ms);
+	add_topic("simulink_outbound_2", sm_log_delay_ms);
+	add_topic("simulink_outbound_3", sm_log_delay_ms);
+	add_topic("simulink_inbound_ctrl", sm_log_delay_ms);
+	add_topic("simulink_guidance", sm_log_delay_ms);
+
+	//CAN topics:
+	// add_topic("actuator_outputs_sv", 20);
+	// add_topic("actuator_outputs_esc", 20);
+
+	//get the guidance topics:
+	int32_t compg_log_delay_ms = 0;
+	param_get(param_find("COMPG_LOG_DELAY"),&compg_log_delay_ms);
+	add_topic("sim_guidance_request");
+	add_topic("sim_guidance_status", 20);
+	add_topic("sim_guidance_trajectory", compg_log_delay_ms);
+	add_topic("companion_guidance_inbound", compg_log_delay_ms);
+	add_topic("companion_guidance_outbound", compg_log_delay_ms);
+
+}
+
 int LoggedTopics::add_topics_from_file(const char *fname)
 {
 	int ntopics = 0;
@@ -599,5 +775,9 @@ void LoggedTopics::initialize_configured_topics(SDLogProfileMask profile)
 
 	if (profile & SDLogProfileMask::MAVLINK_TUNNEL) {
 		add_mavlink_tunnel();
+	}
+
+	if (profile & SDLogProfileMask::SIMULINK) {
+		add_sim_topics();
 	}
 }
